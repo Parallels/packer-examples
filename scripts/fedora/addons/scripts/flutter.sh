73331900b8c1ee61861591926abf4c2ea3ca3605
cjlapao <cjlapa@gmail.com>
2023-09-07 06:09:58 +0100

    fixing path

--- scripts/fedora/addons/scripts/flutter.sh
+++ scripts/fedora/addons/scripts/flutter.sh
@@ -6,7 +6,7 @@ install() {
   sudo dnf update -y
   sudo dnf install -y which git wget unzip xz tar lib32stdc++6 curl
 
-  cd /usr/local
+  cd $HOME_DIR
   # Download and extract Flutter SDK
   LATEST=$(curl -s https://storage.googleapis.com/flutter_infra/releases/releases_linux.json | grep version | grep -v dev | grep -v beta | grep -v rc | grep -v pre | head -n 1 | cut -d '"' -f 4)
   echo "Downloading Flutter SDK version $LATEST"
@@ -17,7 +17,7 @@ install() {
 
   echo "Adding Flutter to PATH"
   # Add Flutter to PATH
-  echo 'export PATH="$PATH:/usr/local/flutter/bin"' >>$HOME_DIR/.bashrc
+  echo "export PATH=\"$PATH:/$HOME_DIR/flutter/bin\"" >>$HOME_DIR/.bashrc
   source $HOME_DIR/.bashrc
 
   # Run Flutter doctor to verify installation
